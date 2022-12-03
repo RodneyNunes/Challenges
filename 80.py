e84ee300e2eb70308d6c95fb94632fa38d1a38a9
Rodney Nunes Barbosa <119550204+RodneyNunes@users.noreply.github.com>
2022-12-02 22:58:19 -0300

    Update 80.py

--- 80.py
+++ 80.py
@@ -9,27 +9,3 @@ for c in range(0, 5):
                 lista.insert(cont, n)
                 break
 print(lista)
-
-'''lista = []
-maior = menor = 0
-for c in range(0, 5):
-    n = int(input('Digite um número: '))
-    if c == 0:
-        maior = menor = n
-        lista.append(n)
-    else:
-        if n >= maior:
-            maior = n
-            lista.append(n)
-        elif n <= menor:
-            menor = n
-            lista.insert(0, n)
-        elif menor < n < maior:
-            lista.insert(1, n)
-            if n >= lista[2]:
-                del(lista[1])
-                lista.insert(2, n)
-            elif n >= lista[3]:
-                del(lista[1])
-                lista.insert(3, n)
-print(lista)'''
